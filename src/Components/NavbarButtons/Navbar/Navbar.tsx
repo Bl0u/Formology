97b794b2b0d0d04d91dc41740a7ec402f0ff3709
Peter Emil <123118179+Bl0u@users.noreply.github.com>
2025-04-27 02:29:59 +0300

    Add Navbar component and styling: Created Navbar.tsx for navigation structure and Navbar.css for styling, including layout, logo, and button styles.

---
+++ src/Components/NavbarButtons/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import {ReactNode} from 'react';
+
+
+interface props {
+    children?: ReactNode ;
+}
+export default function Navbar(props: props) {
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-content">
+                <div className="navbar-logo">
+                    <span className="logo-text">LOGO</span>
+                </div>
+                {props.children}
+            </div>
+        </nav>
+    );
+}
